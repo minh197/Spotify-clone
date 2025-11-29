293f67de7a925d60f1e4854606220e3bb42b8a1e
tinworking925 <tin.working9t5@gmail.com>
2025-11-29 22:55:49 +0700

    add reviews

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -10,10 +10,11 @@ export const generateToken = (userId: number): string => {
   if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET is not defined in environment variables");
   }
-
+  // **REVIEW: nên sử dụng process.env.JWT_EXPIRE_IN
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
+  // **END_REVIEW
 };
 
 export const verifyToken = (token: string): JWTPayload | null => {
